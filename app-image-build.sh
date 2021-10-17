baadb72347f534f574ec2587cb030179f7136560
root <root@redhat.development>
2021-10-17 21:00:54 +0200

    poprawki w pliku

--- app-image-build.sh
+++ app-image-build.sh
@@ -2,14 +2,14 @@
 # Build our Flask app and all the dependencies into a container image
 # Note: OOTB on RHEL 7.6 this needs to be run as root.
 
-MYIMAGE=GS/WorkinDaysCallendar
+MYIMAGE=gs/workingdayscallendar
 FLASK_APP=WorkingDaysCallendar
 FLASK_ENV=development
 USERID=1000
 
 IMAGEID=$(buildah from ubi8/python-36)
+buildah run $IMAGEID pip3 install --upgrade pip
 buildah run $IMAGEID pip3 install flask flask-restful
-
 buildah config --env FLASK_APP=$FLASK_APP --env FLASK_ENV=$FLASK_ENV $IMAGEID
 
 # any build steps above this line run as root inside the container
